993c828d8983e15ade77f925b130ccbb165b1238
BrenoCRSilva <brenocrs@gmail.com>
2024-06-17 22:11:09 -0300

    improved readability of get_num_chars()

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    num_char = get_num_characters(text)
+    num_char = get_num_chars(text)
     print(f"{num_words} words found in the document")
     print(num_char)
 
@@ -16,13 +16,13 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def get_num_characters(text):
+def get_num_chars(text):
     char_dict = {}
     char_list = ["a", "b", "c", "d", "e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     lowered_text = text.lower()
     for char in char_list:
         count = lowered_text.count(char)
-        char_dict.update({char: count})
+        char_dict[char] = count
     return char_dict
         
             
